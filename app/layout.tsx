d0405fdc38bdde47b0da8d250354333cd19fab21
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-05-12 01:14:56 +0530

    added graphbase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,20 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ShotCraft",
-  description: "Event ",
+export const metadata = {
+  title: "Flexibble",
+  description: "Showcase and discover remarkable developer projects",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang='en'>
+      <body>
         <Navbar />
-        {children}
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
